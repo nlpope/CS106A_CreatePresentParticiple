0580c4d16baa1b6561e80f3d57973624d29420b7
Noah Pope <npope@tutanota.com>
2026-09-14 04:31:47 -0500

    adding files to new repo

---
+++ CS106A_CreatePresentParticiple.java
@@ -0,0 +1,27 @@
+/**
+ * File: CS106A_CreatePresentParticiple.java 
+ * ------------------------------------------
+ * The CS106A_CreatePresentParticiple class converts a 
+ * String into its present participle / gerund form using
+ * the PSGerund package. This file is simply to test 
+ * PSGerund's functionality.
+ * 
+ */
+
+import acm.program.*;
+import com.popesoftware.test.*;
+
+public class CS106A_CreatePresentParticiple extends ConsoleProgram
+{
+	
+	private static final String SENTINEL = "ENDPROGRAM";
+	public void run()
+	{
+		while (true){
+			String userInput = readLine("enter word:");
+			if (userInput == SENTINEL){ println("bye"); return; }
+			println(PSGerund.createPresentParticiple(userInput));
+		}
+	}
+	
+}
